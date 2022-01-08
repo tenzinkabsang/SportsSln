40f26b373d97558c4a822e43c75025beb5e654b9
Tenzin Kabsang <tenzinkabsang@gmail.com>
2022-01-08 10:54:33 -0700

    Created project structure

---
+++ SportsStore/Models/IStoreRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace SportsStore.Models
+{
+    public interface IStoreRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+
+    public class EFStoreRepository : IStoreRepository
+    {
+        private StoreDbContext _dbContext;
+        public EFStoreRepository(StoreDbContext ctx)
+        {
+            _dbContext = ctx;
+        }
+
+        public IQueryable<Product> Products => _dbContext.Products;
+    }
+}
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,6 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<StoreDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SportsStoreConnection")));
+
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+
+
+var app = builder.Build();
+app.UseDeveloperExceptionPage();
+app.UseStatusCodePages();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+
+app.UseHttpsRedirection();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
